1d620ee35c0b5d6c5930f80699b0b3f7dff40ebb
DelwarHosssain <mddelwarhossainnobin12@gmail.com>
2023-10-09 21:10:50 +0600

    compled-comingsoonevent

--- src/pages/Home/Coming-event/ComingEvent.jsx
+++ src/pages/Home/Coming-event/ComingEvent.jsx
@@ -1,8 +1,86 @@
-
+import '../../../ComingEvent/ComingEvent.css'
+import {AiOutlineClockCircle } from 'react-icons/ai';
+import {FaMapLocation  } from 'react-icons/fa6';
 const ComingEvent = () => {
     return (
         <div>
-            
+            <div className=" text-center mt-20 space-y-5">
+            <h1 className=" text-3xl font-bold">Upcoming Events</h1>
+            <div  className=" w-56  m-auto border  border-[#FFB606]"></div>
+            <p className=" text-base  font-medium">Discourse assurance estimable applauded to so. Him everything melancholy uncommonly but solicitude inhabiting <br /> projection off. Connection stimulated estimating excellence an to impression.z</p>
+            </div>
+            <div className="mt-20">
+                <div className="card card-side bg-base-100  shadow-xl m-auto w-10/12 gap-4">
+                    <figure><img className="h-96" src="https://i.ibb.co/rs0bTkP/1.jpg" alt="Movie"/></figure>
+                    <div className="flex   mt-10 space-x-11   ">
+                    
+                    <div>
+                        <h1 className="  text-7xl text-[#FFB606] font-extrabold">16</h1>
+                        <p className="ml-3 text-[#807c7c]">APR, 2023</p>
+                    </div>
+                    <div className="h-[100px] border-l-[2px] border-l-[#e9edf7] border-solid "></div>
+                    <div className=" space-y-5">
+                        <h1 className=" text-xl font-extrabold">Social Science & Humanities</h1>
+                        <div className=" flex space-x-4">
+                            <AiOutlineClockCircle  className=' text-[#FFB606]'></AiOutlineClockCircle><p>8:00 - 16:00</p>
+                            <div className="h-[20px] border-l-[2px] border-l-[#e9edf7] border-solid "></div>
+                            <FaMapLocation  className=' text-[#FFB606]'></FaMapLocation ><p> California, TX 70240</p>
+                        </div>
+                        <p className="text-justify pr-10">Attachment astonished to on appearance imprudence <br /> so collecting in excellence.Tiled way blind lived whose new. <br /> The for fully had she there leave merit enjoy forth.</p>
+                        <button className=''>Book Now</button>
+                    </div>
+                     
+                    </div>
+                </div>
+            </div>
+            <div className="mt-20">
+                <div className="card card-side bg-base-100  shadow-xl m-auto w-10/12 gap-4">
+                    <figure><img className="h-96" src="https://i.ibb.co/JtcDxgK/2.jpg" alt="Movie"/></figure>
+                    <div className="flex   mt-10 space-x-11   ">
+                    
+                    <div>
+                        <h1 className="  text-7xl text-[#FFB606] font-extrabold">8</h1>
+                        <p className="ml-3 text-[#807c7c]">JUL, 2024</p>
+                    </div>
+                    <div className="h-[100px] border-l-[2px] border-l-[#e9edf7] border-solid "></div>
+                    <div className=" space-y-5">
+                        <h1 className=" text-xl font-extrabold">Secondary Schools United Nations</h1>
+                        <div className=" flex space-x-4">
+                            <AiOutlineClockCircle  className=' text-[#FFB606]'></AiOutlineClockCircle><p>10:00 - 14:00</p>
+                            <div className="h-[20px] border-l-[2px] border-l-[#e9edf7] border-solid "></div>
+                            <FaMapLocation  className=' text-[#FFB606]'></FaMapLocation ><p> California, TX 70240</p>
+                        </div>
+                        <p className="text-justify pr-10">Attachment astonished to on appearance imprudence <br /> so collecting in excellence.Tiled way blind lived whose new. <br /> The for fully had she there leave merit enjoy forth.</p>
+                        <button className=''>Book Now</button>
+                    </div>
+                     
+                    </div>
+                </div>
+            </div>
+            <div className="mt-20">
+                <div className="card card-side bg-base-100  shadow-xl m-auto w-10/12 gap-4">
+                    <figure><img className="h-96" src="https://i.ibb.co/HdF8NDp/3.jpg" alt="Movie"/></figure>
+                    <div className="flex   mt-10 space-x-11   ">
+                    
+                    <div>
+                        <h1 className="  text-7xl text-[#FFB606] font-extrabold">16</h1>
+                        <p className="ml-3 text-[#807c7c]">AUG, 2024</p>
+                    </div>
+                    <div className="h-[100px] border-l-[2px] border-l-[#e9edf7] border-solid "></div>
+                    <div className=" space-y-5">
+                        <h1 className=" text-xl font-extrabold">International Conference on Art </h1>
+                        <div className=" flex space-x-4">
+                            <AiOutlineClockCircle  className=' text-[#FFB606]'></AiOutlineClockCircle><p>11:00 - 19:00</p>
+                            <div className="h-[20px] border-l-[2px] border-l-[#e9edf7] border-solid "></div>
+                            <FaMapLocation  className=' text-[#FFB606]'></FaMapLocation ><p> California, TX 70240</p>
+                        </div>
+                        <p className="text-justify pr-10">Attachment astonished to on appearance imprudence <br /> so collecting in excellence.Tiled way blind lived whose new. <br /> The for fully had she there leave merit enjoy forth.</p>
+                        <button className=''>Book Now</button>
+                    </div>
+                     
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import { AiFillAccountBook } from "react-icons/ai";
 import { FaBookOpen, FaGlobeAfrica} from "react-icons/fa";
 import HomePart1 from './HomePart1/HomePart1';
 import { useLoaderData } from 'react-router-dom';
+import ComingEvent from './Coming-event/ComingEvent';
 
 const Home = () => {
     const catagores =useLoaderData()
@@ -15,7 +16,7 @@ const Home = () => {
             {{backgroundImage: `url(${image})`}}  >
             
             <div className=' space-y-10 py-40   '>
-                <h1 className=' text-6xl  text-center text-white  font-extrabold m-auto w-6/12  '>Growth You Career With  Complate Courses</h1>
+                <h1 className=' text-6xl  text-center text-white  font-extrabold m-auto w-5/12  '>Growth You Career With  Complate Courses</h1>
                 <div className=' space-x-10  text-white flex  justify-center gap-5'>
                 <div className='flex mt-5 '>
                 <AiFillAccountBook className=' text-lg mr-3 text-[#FFB606]' ></AiFillAccountBook><p>ARCHITECTURE</p> 
@@ -30,7 +31,9 @@ const Home = () => {
             </div>
         </div>
             <HomePart1 catagores={catagores}></HomePart1>
-            
+            <div>
+                <ComingEvent></ComingEvent>
+            </div>
         </div>
     );
 };
