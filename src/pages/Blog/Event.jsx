ff153f5463ad3de07e354fad6469ef97a3965091
DelwarHosssain <mddelwarhossainnobin12@gmail.com>
2023-10-16 13:34:36 +0600

    project Edit

--- src/pages/Blog/Event.jsx
+++ src/pages/Blog/Event.jsx
@@ -7,7 +7,7 @@ const Event= () => {
     const {Loading}=useContext(AuthContacex)
     
     return (
-        <div className="mb-10">
+        <div className="mb-10 container mx-auto">
             <ComingEvent></ComingEvent>
         </div>
     );
